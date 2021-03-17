4b083077daba98217d07cf80a3b5c8fa5bc577f8
Casey Idzikowski <idzikoj1234@gmail.com>
2021-03-17 15:54:55 -0500

    new table plugin

--- site-content/backend-plugins/__init__.py
+++ site-content/backend-plugins/__init__.py
@@ -8,22 +8,29 @@ from pathlib import Path
 import json
 
 
-def metrics_view():
-
-    db = app_context().database
-
-    p = Path(relative_path(__file__, "metrics.sql"))
-    sqlfile = open(p, "r")
-    query = sqlfile.read()
-
-    test = pd.read_sql(query, db.engine.connect())
-    
-    res = test.to_json(orient='records')
-
-    return JSONResponse(json.loads(res))
-
 class MetricsEndpointWiscAr(SparrowPlugin):
+    '''
+        This Sparrow Plugin adds a GET route to the API. 
+        It works by reading in a postgreSQL query from a 
+        file in this directory, querying the sparrow 
+        database, and then returning the response a JSON.
+
+        It then uses the on_api_initialized_v2 hook to add the route
+        and some route documentation that shows up on the api.
+    '''
     name = 'metrics'
+
+    def metrics_view(self):
+
+        db = self.app.database
+        p = Path(relative_path(__file__, "metrics.sql"))
+        sqlfile = open(p, "r")
+        query = sqlfile.read()
+
+        metrics = db.exec_query(query)
+        res = metrics.to_json(orient='records')
+
+        return JSONResponse(json.loads(res))
 
     def on_api_initialized_v2(self, api):
 
@@ -32,6 +39,20 @@ class MetricsEndpointWiscAr(SparrowPlugin):
             route = "/metrics",
             description = "A metrics route for WiscAr Lab",
         )
-        api.add_route("/metrics", metrics_view(), methods=['GET'], include_in_schema=False)
+        api.add_route("/metrics", self.metrics_view(), methods=['GET'], include_in_schema=False)
         api.route_descriptions[root_route].append(basic_info)
 
+
+class AddNewTable(SparrowPlugin):
+    '''
+        This plugin's purpose is to create a new table in the database.
+
+        An example of how a lab can add a table for unqiue data models.
+        TODO: How does this then get added to the api?
+    '''
+    name = 'favorite-rock'
+
+    def on_database_ready(self, db):
+
+        p = Path(relative_path(__file__, "favorite_rock.sql"))
+        db.exec_sql(p)
---
+++ site-content/backend-plugins/favorite_rock.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS favorite_rock(
+id serial PRIMARY KEY,
+name text,
+favorite_rock text
+);
